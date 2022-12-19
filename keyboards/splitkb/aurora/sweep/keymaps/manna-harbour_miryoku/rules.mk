189b725a54d5b4f23f97851b49f0ee4ab47b39d8
Henrik Pejer <mrpejer@gmail.com>
2022-12-19 08:57:34 +0100

    pejer: hack to make miryoku work w. Sweep. Hope noone sees this...

---
+++ keyboards/splitkb/aurora/sweep/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,7 @@
+# Copyright 2021 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+MIRYOKU_KLUDGE_THUMBCOMBOS = yes
+ENCODER_ENABLE             = no
+BOOTMAGIC_ENABLE           = no
+RGB_MATRIX_ENABLE          = no
